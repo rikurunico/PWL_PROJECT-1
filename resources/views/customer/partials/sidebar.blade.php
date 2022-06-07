f73f579f0bbb254a599732d1e5fd2a5e247db01e
rifarosyida <rifarosyida2002@gmail.com>
2022-06-07 20:31:27 +0700

    edit view customer

--- resources/views/customer/partials/sidebar.blade.php
+++ resources/views/customer/partials/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="{{ asset('src/img/brandlogo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Toko Fast</span>
+      <img src="{{ asset('storage/images/logo.png') }}" alt="Diza Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Diza Shop</span>
     </a>
 
     <!-- Sidebar -->
@@ -10,10 +10,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ auth()->user()->foto }}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset(auth()->user()->foto_profil) }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+          <a href="#" class="d-block">{{ auth()->user()->username }}</a>
         </div>
       </div>
 
@@ -62,31 +62,17 @@
             <a href="#" class="nav-link">
               <i class="nav-icon far fa-image"></i>
               <p>
-              Gallery Product
+              Produk
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link {{ ($tittle === "Gallery Page") ? 'active' : ''}}"">
+                <a href="#" class="nav-link {{ ($tittle === "Produk") ? 'active' : ''}}">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Bahan Dapur</p>
+                  <p>Data Produk</p>
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="pages/charts/flot.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Perlengkapan Mandi</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/charts/inline.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Makanan Instan</p>
-                </a>
-              </li>
-            </ul>
-          </li>
           
           <li class="nav-item">
             <a href="" class="nav-link">
